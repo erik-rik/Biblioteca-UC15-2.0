f4de26d56fcc9f9123f4aa0979187322392a2994
Erik Richter <erik.knabach@gmail.com>
2026-03-15 15:28:13 -0300

    Revert "create EmprestimoUtilsTeste"
    This reverts commit 794172ddb1ff83695722f97f7fb16030f1f68623.

--- Biblioteca (UC15)/src/biblioteca/uc15/service/EmprestimoUtils.java
+++ Biblioteca (UC15)/src/biblioteca/uc15/service/EmprestimoUtils.java
@@ -4,19 +4,10 @@
  */
 package biblioteca.uc15.service;
 
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
-
 /**
  *
  * @author PAULO
  */
 public class EmprestimoUtils {
     
-    public static long calcularDiasEmprestimo(Date dataEmprestimo, Date dataDevolucao) {
-
-        long diferenca = dataDevolucao.getTime() - dataEmprestimo.getTime();
-
-        return TimeUnit.DAYS.convert(diferenca, TimeUnit.MILLISECONDS);
-    }
 }
